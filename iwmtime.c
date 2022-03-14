51be56e2a3f850dbf7ee9a1dcf628c1455cc3fde
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2022-03-14 13:39:12 +0900

    Add files via upload

--- iwmtime.c
+++ iwmtime.c
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
-#define  IWM_VERSION         "iwmtime_20211201"
-#define  IWM_COPYRIGHT       "Copyright (C)2021 iwm-iwama"
+#define  IWM_VERSION         "iwmtime_20220313"
+#define  IWM_COPYRIGHT       "Copyright (C)2021-2022 iwm-iwama"
 //------------------------------------------------------------------------------
 #include "lib_iwmutil.h"
 
@@ -8,31 +8,29 @@ INT  main();
 VOID print_version();
 VOID print_help();
 
-// [文字色] + ([背景色] * 16)
-//  0 = Black    1 = Navy     2 = Green    3 = Teal
-//  4 = Maroon   5 = Purple   6 = Olive    7 = Silver
-//  8 = Gray     9 = Blue    10 = Lime    11 = Aqua
-// 12 = Red     13 = Fuchsia 14 = Yellow  15 = White
-
-// タイトル
-#define  COLOR01             (15 + ( 9 * 16))
+// リセット
+#define  PRGB00()            P0("\033[0m")
+// ラベル
+#define  PRGB01()            P0("\033[38;2;255;255;0m")    // 黄
+#define  PRGB02()            P0("\033[38;2;255;255;255m")  // 白
 // 入力例／注
-#define  COLOR11             (15 + (12 * 16))
-#define  COLOR12             (13 + ( 0 * 16))
-#define  COLOR13             (12 + ( 0 * 16))
+#define  PRGB11()            P0("\033[38;2;255;255;100m")  // 黄
+#define  PRGB12()            P0("\033[38;2;255;220;150m")  // 橙
+#define  PRGB13()            P0("\033[38;2;100;100;255m")  // 青
 // オプション
-#define  COLOR21             (14 + ( 0 * 16))
-#define  COLOR22             (11 + ( 0 * 16))
-// 説明
-#define  COLOR91             (15 + ( 0 * 16))
-#define  COLOR92             ( 7 + ( 0 * 16))
+#define  PRGB21()            P0("\033[38;2;80;255;255m")   // 水
+#define  PRGB22()            P0("\033[38;2;255;100;255m")  // 紅紫
+// 本文
+#define  PRGB91()            P0("\033[38;2;255;255;255m")  // 白
+#define  PRGB92()            P0("\033[38;2;200;200;200m")  // 銀
 
 INT
 main()
 {
 	// lib_iwmutil 初期化
-	iCLI_getARGV();      //=> $CMD, $ARGV, $ARGC
-	iConsole_getColor(); //=> $ColorDefault, $StdoutHandle
+	iExecSec_init();  //=> $ExecSecBgn
+	iCLI_getARGV();   //=> $CMD, $ARGV, $ARGC
+	iConsole_EscOn();
 
 	// -h | -help
 	if(! $ARGC || iCLI_getOptMatch(0, "-h", "-help"))
@@ -64,13 +62,10 @@ main()
 	GlobalMemoryStatusEx(&memex);
 	iBgnEmpMem = memex.ullAvailPhys;
 
-	// 計測開始
-	iExecSec_init(); //=> $ExecSecBgn
-
 	if(iOptionQuiet)
 	{
 		system(ims_cats(3, (cmd + iOptionQuiet), " > ", NULL_DEVICE));
-		PZ(COLOR21, NULL);
+		PRGB21();
 		P2("[Quiet Mode]");
 	}
 	else
@@ -84,7 +79,7 @@ main()
 	GlobalMemoryStatusEx(&memex);
 	iEndEmpMem = memex.ullAvailPhys;
 
-	PZ(COLOR91, NULL);
+	PRGB91();
 	LN();
 	// Program
 	P("  Program  %s\n", (cmd + iOptionQuiet));
@@ -97,7 +92,7 @@ main()
 	// Exec
 	P("  Exec     %s sec\n", ims_addTokenNStr(ims_sprintf("%.4f", dPassedSec)));
 	LN();
-	PZ(-1, NULL);
+	PRGB00();
 
 	// Debug
 	/// icalloc_mapPrint(); ifree_all(); icalloc_mapPrint();
@@ -109,28 +104,38 @@ main()
 VOID
 print_version()
 {
-	PZ(COLOR92, NULL);
+	PRGB92();
 	LN();
-	P(" %s\n", IWM_COPYRIGHT);
-	P("   Ver.%s+%s\n", IWM_VERSION, LIB_IWMUTIL_VERSION);
+	P (" %s\n", IWM_COPYRIGHT);
+	P ("   Ver.%s+%s\n", IWM_VERSION, LIB_IWMUTIL_VERSION);
 	LN();
-	PZ(-1, NULL);
+	PRGB00();
 }
 
 VOID
 print_help()
 {
 	print_version();
-	PZ(COLOR01, " コマンドの実行時間を計測 \n\n");
-	PZ(COLOR11, " %s [コマンド] [オプション] [引数] ... \n\n", $CMD);
-	PZ(COLOR12, " (例１)\n");
-	PZ(COLOR91, "   > %s notepad\n\n", $CMD);
-	PZ(COLOR12, " (例２)\n");
-	PZ(COLOR91, "   > %s dir \"..\" /b\n\n", $CMD);
-	PZ(COLOR21, " [オプション]\n");
-	PZ(COLOR22, "   -quiet | -q\n");
-	PZ(COLOR91, "       コマンド出力を表\示しない\n\n");
-	PZ(COLOR92, NULL);
+	PRGB01();
+	P2("\033[48;2;80;80;250m コマンドの実行時間を計測 \033[49m");
+	NL();
+	PRGB02();
+	P ("\033[48;2;250;80;80m %s [Option] [Command] \033[49m\n\n", $CMD);
+	PRGB11();
+	P0(" (例１) ");
+	PRGB91();
+	P ("%s \033[38;2;255;150;150mnotepad\n\n", $CMD);
+	PRGB11();
+	P0(" (例２) ");
+	PRGB91();
+	P ("%s \033[38;2;255;150;150mdir \033[38;2;150;150;255m\"..\" /b\n\n", $CMD);
+	PRGB02();
+	P2("\033[48;2;250;80;80m [Option] \033[49m");
+	PRGB21();
+	P2("   -quiet | -q");
+	PRGB91();
+	P2("       コマンド出力を表\示しない");
+	PRGB92();
 	LN();
-	PZ(-1, NULL);
+	PRGB00();
 }
