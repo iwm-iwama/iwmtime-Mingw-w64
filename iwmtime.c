61bec50bb539f14283be947e5ae3acb6136f98d6
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2022-09-11 12:45:19 +0900

    Add files via upload

--- iwmtime.c
+++ iwmtime.c
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-#define  IWM_VERSION         "iwmtime_20220905"
+#define  IWM_VERSION         "iwmtime_20220910"
 #define  IWM_COPYRIGHT       "Copyright (C)2021-2022 iwm-iwama"
 //------------------------------------------------------------------------------
 #include "lib_iwmutil2.h"
@@ -9,20 +9,20 @@ VOID print_version();
 VOID print_help();
 
 // リセット
-#define  PRGB00()            P0("\x1b[0m")
+#define  PRGB00()            P0("\033[0m")
 // ラベル
-#define  PRGB01()            P0("\x1b[38;2;255;255;0m")    // 黄
-#define  PRGB02()            P0("\x1b[38;2;255;255;255m")  // 白
+#define  PRGB01()            P0("\033[38;2;255;255;0m")    // 黄
+#define  PRGB02()            P0("\033[38;2;255;255;255m")  // 白
 // 入力例／注
-#define  PRGB11()            P0("\x1b[38;2;255;255;100m")  // 黄
-#define  PRGB12()            P0("\x1b[38;2;255;220;150m")  // 橙
-#define  PRGB13()            P0("\x1b[38;2;100;100;255m")  // 青
+#define  PRGB11()            P0("\033[38;2;255;255;100m")  // 黄
+#define  PRGB12()            P0("\033[38;2;255;220;150m")  // 橙
+#define  PRGB13()            P0("\033[38;2;100;100;255m")  // 青
 // オプション
-#define  PRGB21()            P0("\x1b[38;2;80;255;255m")   // 水
-#define  PRGB22()            P0("\x1b[38;2;255;100;255m")  // 紅紫
+#define  PRGB21()            P0("\033[38;2;80;255;255m")   // 水
+#define  PRGB22()            P0("\033[38;2;255;100;255m")  // 紅紫
 // 本文
-#define  PRGB91()            P0("\x1b[38;2;255;255;255m")  // 白
-#define  PRGB92()            P0("\x1b[38;2;200;200;200m")  // 銀
+#define  PRGB91()            P0("\033[38;2;255;255;255m")  // 白
+#define  PRGB92()            P0("\033[38;2;200;200;200m")  // 銀
 
 INT
 main()
@@ -162,20 +162,20 @@ print_help()
 
 	print_version();
 	PRGB01();
-	P2("\x1b[48;2;50;50;200m コマンドの実行時間を計測 \x1b[0m");
+	P2("\033[48;2;50;50;200m コマンドの実行時間を計測 \033[0m");
 	NL();
 	PRGB02();
-	P ("\x1b[48;2;200;50;50m %s [Option] [Command] \x1b[0m\n\n", _cmd);
+	P ("\033[48;2;200;50;50m %s [Option] [Command] \033[0m\n\n", _cmd);
 	PRGB11();
 	P0(" (例１) ");
 	PRGB91();
-	P ("%s \x1b[38;2;150;150;255mnotepad\n\n", _cmd);
+	P ("%s \033[38;2;150;150;255mnotepad\n\n", _cmd);
 	PRGB11();
 	P0(" (例２) ");
 	PRGB91();
-	P ("%s \x1b[38;2;255;150;150m-quiet \x1b[38;2;150;150;255mdir \"..\" /b\n\n", _cmd);
+	P ("%s \033[38;2;255;150;150m-quiet \033[38;2;150;150;255mdir \"..\" /b\n\n", _cmd);
 	PRGB02();
-	P2("\x1b[48;2;200;50;50m [Option] \x1b[0m");
+	P2("\033[48;2;200;50;50m [Option] \033[0m");
 	PRGB21();
 	P2("   -quiet | -q");
 	PRGB91();
