bc349de1e7ca31cfee66de415f60b8011d70d2e9
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2024-03-11 11:57:22 +0900

    Add files via upload

--- iwmtime.c
+++ iwmtime.c
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 #define   IWM_COPYRIGHT       "(C)2021-2024 iwm-iwama"
-#define   IWM_VERSION         "iwmtime_20240118"
+#define   IWM_VERSION         "iwmtime_20240310"
 //------------------------------------------------------------------------------
 #include "lib_iwmutil2.h"
 #include <psapi.h>
@@ -35,7 +35,6 @@ main()
 	}
 
 	WS *pARG = $ARG;
-	WS *wpCmd = 0;
 
 	// -c | -cmd
 	if(iCLI_getOptMatch(0, L"-c", L"-cmd"))
@@ -49,57 +48,56 @@ main()
 		{
 			pARG += 5;
 		}
-		wpCmd = iws_cats(2, L"cmd.exe /c ", pARG);
-	}
-	else
-	{
-		wpCmd = iws_clone(pARG);
 	}
 
-	STARTUPINFOW si;
-		ZeroMemory(&si, sizeof(si));
-		si.cb = sizeof(si);
-	PROCESS_INFORMATION pi;
-		ZeroMemory(&pi, sizeof(pi));
+	WS *wpCmd = iws_cats(2, L"cmd.exe /c ", pARG);
 
-	if(CreateProcessW(NULL, wpCmd, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
-	{
-		WaitForSingleObject(pi.hProcess, INFINITE);
+		STARTUPINFOW si;
+			ZeroMemory(&si, sizeof(si));
+			si.cb = sizeof(si);
 
-		HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pi.dwProcessId);
-			FILETIME creationTime, exitTime, kernelTime, userTime;
-			GetProcessTimes(hProcess, &creationTime, &exitTime, &kernelTime, &userTime);
-			PROCESS_MEMORY_COUNTERS pmc;
-			GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc));
-			MS *mpCmd = W2M(wpCmd);
-				SetConsoleOutputCP(65001);
-				P1("\033[92m");
-				LN(80);
-				P(
-					"\033[92m"	"  Program"	"\033[96m"	"  %s\n"
-					"\033[92m"	"  Execute"	"\033[96m"	"  %.3f sec\n"
-					"\033[92m"	"  Memory "	"\033[96m"	"  %.3f MB\n"
-					,
-					mpCmd,
-					((iFinfo_ftimeToINT64(exitTime) - iFinfo_ftimeToINT64(creationTime)) / NANO100),
-					(pmc.PeakPagefileUsage / MB)
-				);
-				P1("\033[92m");
-				LN(80);
-				P2(IESC_RESET);
-			ifree(mpCmd);
-		CloseHandle(hProcess);
-	}
-	else
-	{
-		P2(IESC_FALSE1 "[Err] コマンドを確認してください!" IESC_RESET);
-	}
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
+		PROCESS_INFORMATION pi;
+			ZeroMemory(&pi, sizeof(pi));
+
+		if(CreateProcessW(NULL, wpCmd, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+		{
+			WaitForSingleObject(pi.hProcess, INFINITE);
+
+			HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pi.dwProcessId);
+				FILETIME creationTime, exitTime, kernelTime, userTime;
+				GetProcessTimes(hProcess, &creationTime, &exitTime, &kernelTime, &userTime);
+				PROCESS_MEMORY_COUNTERS pmc;
+				GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc));
+				MS *mpCmd = W2M(pARG);
+					SetConsoleOutputCP(65001);
+					P1("\033[92m");
+					LN(80);
+					P(
+						"\033[92m"	"  Program"	"\033[96m"	"  %s\n"
+						"\033[92m"	"  Execute"	"\033[96m"	"  %.3f sec\n"
+						"\033[92m"	"  Memory "	"\033[96m"	"  %.3f MB\n"
+						,
+						mpCmd,
+						((iFinfo_ftimeToINT64(exitTime) - iFinfo_ftimeToINT64(creationTime)) / NANO100),
+						(pmc.PeakPagefileUsage / MB)
+					);
+					P1("\033[92m");
+					LN(80);
+					P2(IESC_RESET);
+				ifree(mpCmd);
+			CloseHandle(hProcess);
+		}
+		else
+		{
+			P2(IESC_FALSE1 "[Err] コマンドを確認してください!" IESC_RESET);
+		}
+
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
 
 	ifree(wpCmd);
 
-	///icalloc_mapPrint(); ifree_all(); icalloc_mapPrint();
+	///idebug_map(); ifree_all(); idebug_map();
 
 	// 最終処理
 	imain_end();
@@ -131,25 +129,13 @@ print_help()
 	P(
 		IESC_TITLE1	" コマンドの実行時間を計測 "	IESC_RESET	"\n\n"
 		IESC_STR1	"    %s"
-		IESC_OPT2	" [Option]"
 		IESC_OPT1	" [Command]\n\n\n"
-		IESC_LBL1	" (例１)\n"
+		IESC_LBL1	" (例)\n"
 		IESC_STR1	"    %s"
-		IESC_OPT1	" notepad.exe\n\n"
-		IESC_LBL1	" (例２)\n"
-		IESC_STR1	"    %s"
-		IESC_OPT2	" -cmd"
-		IESC_OPT1	" dir /b\n\n\n"
+		IESC_OPT1	" dir /b\n\n"
 		,
 		_cmd,
-		_cmd,
 		_cmd
-	);
-	P1(
-		IESC_OPT2	" [Option]\n"
-		IESC_OPT21	"    -cmd | -c\n"
-		IESC_OPT1	"        cmd.exe /c [Command]"
-		IESC_STR1	" を実行\n\n"
 	);
 	P1(IESC_STR2);
 	LN(80);
